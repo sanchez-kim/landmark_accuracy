9ea945c74d724288be77be167b869590047548f5
Sanchez Kim <sanchez.kim.kr@gmail.com>
2023-11-06 11:20:19 +0900

    update

--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OBJLoader } from "OBJLoader";
+import { degToRad } from "MathUtils";
 
 const scene = new THREE.Scene();
 const canvas = document.getElementById("webgl_canvas");
@@ -17,7 +18,7 @@ camera.fov = 5;
 camera.updateProjectionMatrix();
 
 const light = new THREE.DirectionalLight(0xffffff, 2.0);
-light.position.set(1, 1, 1);
+light.position.set(1, 1, 1).normalize();
 scene.add(light);
 
 window.addEventListener("resize", function () {
@@ -35,6 +36,7 @@ let landmarks2 = null;
 let canvasCtx = null;
 let isImageCanvas = null;
 let storedImage = null;
+let currentCtx;
 
 const imgCanvas = document.getElementById("image_canvas");
 const imgCtx = imgCanvas.getContext("2d");
@@ -42,17 +44,77 @@ const imgCtx = imgCanvas.getContext("2d");
 const renderCanvas = document.getElementById("render_canvas");
 const renderCtx = renderCanvas.getContext("2d");
 
-function resizeLandmarks(landmarks, box, newWidth, newHeight) {
-  const originalWidth = box.maxX - box.minX;
-  const originalHeight = box.maxY - box.minY;
+const faceDetection = new FaceDetection({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_detection/${file}`;
+  },
+});
 
-  const widthScale = newWidth / originalWidth;
-  const heightScale = newHeight / originalHeight;
+faceDetection.setOptions({
+  model: "short",
+  maxNumFaces: 1, // Detect only one face
+  minDetectionConfidence: 0.5,
+});
 
-  return landmarks.map((landmark) => ({
-    x: (landmark.x - box.minX) * widthScale,
-    y: (landmark.y - box.minY) * heightScale,
-  }));
+faceDetection.onResults(onFaceDetectionResults);
+
+function onFaceDetectionResults(results) {
+  let canvas, ctx;
+  console.log(currentCtx);
+  if (currentCtx === "REAL") {
+    canvas = imgCanvas;
+    ctx = imgCtx;
+  } else if (currentCtx === "RENDERED") {
+    canvas = renderCanvas;
+    ctx = renderCtx;
+  } else {
+    console.error("Unknown image context: ", currentCtx);
+    return;
+  }
+
+  if (results.detections) {
+    const faceBox = results.detections[0].boundingBox;
+    const croppedFace = cropFace(faceBox, canvas, ctx);
+
+    storedImage = croppedFace;
+
+    faceMesh
+      .send({ image: croppedFace })
+      .then(() => {})
+      .catch((err) => {
+        console.error("Error in faceMesh.send: ", err);
+      });
+  }
+}
+
+function cropFace(faceBox, canvas, ctx, offset = 0.3) {
+  const sourceWidth = canvas.width;
+  const sourceHeight = canvas.height;
+  let x = (faceBox.xCenter - faceBox.width / 2) * sourceWidth;
+  let y = (faceBox.yCenter - faceBox.height / 2) * sourceHeight;
+  let width = faceBox.width * sourceWidth;
+  let height = faceBox.height * sourceHeight;
+
+  const xOffset = width * offset;
+  const yOffset = height * offset;
+
+  x = Math.max(0, x - xOffset);
+  y = Math.max(0, y - yOffset);
+  width = Math.min(sourceWidth - x, width + 2 * xOffset);
+  height = Math.min(sourceHeight - y, height + 2 * yOffset);
+
+  const croppedCanvas = document.createElement("canvas");
+  const croppedCtx = croppedCanvas.getContext("2d");
+
+  croppedCanvas.width = width;
+  croppedCanvas.height = height;
+
+  croppedCtx.drawImage(canvas, x, y, width, height, 0, 0, width, height);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(croppedCanvas, 0, 0, sourceWidth, sourceHeight);
+
+  return canvas;
 }
 
 function onResults(results) {
@@ -61,80 +123,63 @@ function onResults(results) {
     results.multiFaceLandmarks &&
     results.multiFaceLandmarks.length > 0
   ) {
-    const box = getBoundingBox(results.multiFaceLandmarks[0]);
-    const resizedLandmarks = resizeLandmarks(
-      results.multiFaceLandmarks[0],
-      box,
-      640,
-      640
-    );
-
     if (isImageCanvas) {
       canvasCtx = imgCtx;
-      landmarks1 = resizedLandmarks;
+      landmarks1 = results.multiFaceLandmarks[0];
     } else {
+      console.log("Doing render context");
       canvasCtx = renderCtx;
-      landmarks2 = resizedLandmarks;
-    }
+      landmarks2 = results.multiFaceLandmarks[0];
 
-    const {
-      croppedCanvas: croppedCanvas,
-      newWidth,
-      newHeight,
-    } = resizeToTarget(box, imgCanvas, imgCtx);
-    canvasCtx.clearRect(0, 0, imgCanvas.width, imgCanvas.height);
-    canvasCtx.drawImage(croppedCanvas, 0, 0);
-
-    if (storedImage) {
-      canvasCtx.drawImage(
-        storedImage,
-        0,
-        0,
-        canvasCtx.canvas.width,
-        canvasCtx.canvas.height
-      );
+      try {
+        canvasCtx.drawImage(
+          storedImage,
+          0,
+          0,
+          renderCanvas.width,
+          renderCanvas.height
+        );
+      } catch (error) {
+        console.error("Error drawing Mesh: ", error);
+      }
     }
-    // Visualize landmarks on cropped image
-    drawLandmarks(canvasCtx, results.multiFaceLandmarks);
 
     if (landmarks1 && landmarks2) {
       compareLandmarks();
     }
+
+    for (const landmarks of results.multiFaceLandmarks) {
+      drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION, {
+        color: "#C0C0C070",
+        lineWidth: 1,
+      });
+      drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {
+        color: "#FF3030",
+      });
+      drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {
+        color: "#FF3030",
+      });
+      drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, {
+        color: "#FF3030",
+      });
+      drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, {
+        color: "#30FF30",
+      });
+      drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, {
+        color: "#30FF30",
+      });
+      drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, {
+        color: "#30FF30",
+      });
+      drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, {
+        color: "#E0E0E0",
+      });
+      drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {
+        color: "#E0E0E0",
+      });
+    }
   } else {
     console.error("No results!");
-  }
-}
-
-function drawLandmarks(ctx, landmarks) {
-  for (const landmark of landmarks) {
-    drawConnectors(ctx, landmark, FACEMESH_TESSELATION, {
-      color: "#C0C0C070",
-      lineWidth: 1,
-    });
-    drawConnectors(ctx, landmark, FACEMESH_RIGHT_EYE, {
-      color: "#FF3030",
-    });
-    drawConnectors(ctx, landmark, FACEMESH_RIGHT_EYEBROW, {
-      color: "#FF3030",
-    });
-    drawConnectors(ctx, landmark, FACEMESH_RIGHT_IRIS, {
-      color: "#FF3030",
-    });
-    drawConnectors(ctx, landmark, FACEMESH_LEFT_EYE, {
-      color: "#30FF30",
-    });
-    drawConnectors(ctx, landmark, FACEMESH_LEFT_EYEBROW, {
-      color: "#30FF30",
-    });
-    drawConnectors(ctx, landmark, FACEMESH_LEFT_IRIS, {
-      color: "#30FF30",
-    });
-    drawConnectors(ctx, landmark, FACEMESH_FACE_OVAL, {
-      color: "#E0E0E0",
-    });
-    drawConnectors(ctx, landmark, FACEMESH_LIPS, {
-      color: "#E0E0E0",
-    });
   }
 }
 
@@ -145,9 +190,8 @@ function processImage(file, callback) {
   image.onload = function () {
     imgCtx.clearRect(0, 0, imgCanvas.width, imgCanvas.height);
     imgCtx.drawImage(image, 0, 0, imgCanvas.width, imgCanvas.height);
-    storedImage = image;
     try {
-      faceMesh.send({ image: image }).then(() => {
+      faceDetection.send({ image: image }).then((results) => {
         callback();
       });
     } catch (error) {
@@ -172,50 +216,14 @@ async function renderFrontalImage() {
     const image = await canvasToImage(renderer.domElement);
     renderCtx.drawImage(image, 0, 0, renderCanvas.width, renderCanvas.height);
 
-    storedImage = image;
+    const results = await faceDetection.send({ image: image });
 
-    const results = await faceMesh.send({ image: image });
+    if (results && results.detections && results.detection.length > 0) {
+      const faceBox = results.detections[0].boundingBox;
+      const croppedRenderedFace = cropFace(faceBox, renderCanvas, renderCtx);
 
-    if (
-      results &&
-      results.multiFaceLandmarks &&
-      results.multiFaceLandmarks.length > 0
-    ) {
-      const box = getBoundingBox(results.multiFaceLandmarks[0]);
-
-      const resizedLandmarks = resizeLandmarks(
-        results.multiFaceLandmarks[0],
-        box,
-        1000,
-        1000
-      );
-      landmarks2 = resizedLandmarks;
-
-      console.log(`Original bounding box width: ${box.maxX - box.minX} pixels`);
-      console.log(
-        `Original bounding box height: ${box.maxY - box.minY} pixels`
-      );
-
-      const {
-        croppedCanvas: croppedRenderedFace,
-        newWidth,
-        newHeight,
-      } = resizeToTarget(box, renderCanvas, renderCtx);
-
-      // Logging the resized dimensions
-      console.log(`Resized width: ${newWidth} pixels`);
-      console.log(`Resized height: ${newHeight} pixels`);
-
-      renderCtx.clearRect(0, 0, renderCanvas.width, renderCanvas.height);
-      renderCtx.drawImage(
-        croppedRenderedFace,
-        box.minX,
-        box.minY,
-        newWidth,
-        newHeight
-      );
-
-      if (landmarks1 && landmarks2) {
+      if (faceMesh) {
+        await faceMesh.send({ image: croppedRenderedFace });
         compareLandmarks();
       }
     }
@@ -226,25 +234,37 @@ async function renderFrontalImage() {
 
 function compareLandmarks() {
   let total = 0;
-  let num_landmarks = landmarks1.length;
+  let num_landmarks;
   let detailedMessage = "";
 
-  for (let i = 0; i < num_landmarks; i++) {
-    let distance = Math.sqrt(
-      Math.pow(landmarks2[i].x - landmarks1[i].x, 2) +
-        Math.pow(landmarks2[i].y - landmarks1[i].y, 2)
-    );
-    total += distance;
-    detailedMessage += `Distance for landmark ${i}: ${distance.toFixed(2)}\n`;
+  if (!landmarks1 || !landmarks2) {
+    return;
   }
 
-  const average = total / num_landmarks;
-  const message = `
-  Number of Landmarks: ${num_landmarks}
-  Average Distance: ${average.toFixed(2)} pixels
-  `;
-  document.querySelector(".result").innerText = message;
-  document.getElementById("detailed_results").innerText = detailedMessage;
+  if (landmarks1 && landmarks2) {
+    num_landmarks = landmarks1.length;
+    // Calculate distances between corresponding landmarks.
+    for (let i = 0; i < landmarks1.length; i++) {
+      let x1_pixel = landmarks1[i].x * canvas.width;
+      let y1_pixel = landmarks1[i].y * canvas.height;
+
+      let x2_pixel = landmarks2[i].x * canvas.width;
+      let y2_pixel = landmarks2[i].y * canvas.height;
+
+      const distance = Math.sqrt(
+        Math.pow(x2_pixel - x1_pixel, 2) + Math.pow(y2_pixel - y1_pixel, 2)
+      );
+      total += distance;
+      detailedMessage += `Distance for landmark ${i}: ${distance.toFixed(2)}\n`;
+    }
+    const average = total / num_landmarks;
+    const message = `
+    Number of Landmarks: ${num_landmarks}
+    Average Distance: ${average.toFixed(2)} pixels
+    `;
+    document.querySelector(".result").innerText = message;
+    document.getElementById("detailed_results").innerText = detailedMessage;
+  }
 }
 
 function resetFaceMesh() {
@@ -267,66 +287,6 @@ function resetFaceMesh() {
   faceMesh.onResults(onResults);
 }
 
-function getBoundingBox(landmarks) {
-  let minX = Infinity;
-  let minY = Infinity;
-  let maxX = -Infinity;
-  let maxY = -Infinity;
-
-  landmarks.forEach((point) => {
-    if (point.x < minX) minX = point.x;
-    if (point.y < minY) minY = point.y;
-    if (point.x > maxX) maxX = point.x;
-    if (point.y > maxY) maxY = point.y;
-  });
-
-  return { minX, minY, maxX, maxY };
-}
-
-function transformLandmarksToTarget(landmarks, box) {
-  const width = box.maxX - box.minX;
-  const height = box.maxY - box.minY;
-
-  const newWidth = 1000;
-  const newHeight = 1000;
-
-  const scaleX = newWidth / width;
-  const scaleY = newHeight / height;
-
-  return landmarks.map((landmark) => ({
-    x: (landmark.x - box.minX) * scaleX,
-    y: (landmark.y - box.minY) * scaleY,
-  }));
-}
-
-function resizeToTarget(box, canvas, ctx) {
-  const width = box.maxX - box.minX;
-  const height = box.maxY - box.minY;
-
-  const newWidth = 2000;
-  const newHeight = 2000;
-
-  const croppedCanvas = document.createElement("canvas");
-  const croppedCtx = croppedCanvas.getContext("2d");
-
-  croppedCanvas.width = newWidth;
-  croppedCanvas.height = newHeight;
-  croppedCtx.clearRect(0, 0, newWidth, newHeight);
-  croppedCtx.drawImage(
-    canvas,
-    box.minX,
-    box.minY,
-    width,
-    height,
-    0,
-    0,
-    newWidth,
-    newHeight
-  );
-
-  return { croppedCanvas, newWidth, newHeight };
-}
-
 resetFaceMesh();
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -338,7 +298,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.getElementById("objInput").addEventListener("change", function () {
     resetFaceMesh();
-
+    console.log("Obj canvas triggered");
     isImageCanvas = false;
     currentCtx = "RENDERED";
     const file = this.files[0];
