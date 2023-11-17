8b5c5504f82964164f7a8d92693f4b33143b1300
Sanchez Kim <sanchez.kim.kr@gmail.com>
2023-11-17 11:22:24 +0900

    update lighting and cam settings

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 - node v16.16.0에서 테스트함
 - `npm install`로 패키지 설치
 - `npm run start` 로 프로그램 실행
+
+
+## TODO
+- [x] update light & camera settings
+- [ ] automatic evaluation via puppeteer (on progress)
---
+++ evaluate.js
@@ -0,0 +1,89 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+const sharp = require("sharp");
+
+function readFileAsDataURL(filePath) {
+  const imageBuffer = fs.readFileSync(filePath);
+  return "data:image/png;base64," + imageBuffer.toString("base64");
+}
+
+function readObjAsDataURL(filePath) {
+  const fileBuffer = fs.readFileSync(filePath);
+  const base64 = fileBuffer.toString("base64");
+  return `data:application/octet-stream;base64,${base64}`;
+}
+
+async function fetchAndRotateImage(imageUrl) {
+  const response = await axios({ url: imageUrl, responseType: "arraybuffer" });
+  const rotatedImageBuffer = await sharp(response.data).rotate(-90).toBuffer();
+  fs.writeFileSync("tempImage.png", rotatedImageBuffer);
+}
+
+async function fetchOBJFile(objUrl) {
+  const response = await axios({ url: objUrl, responseType: "arraybuffer" });
+  fs.writeFileSync("tempObj.obj", response.data);
+}
+
+async function calculateLandmarkDistances(imageUrl, objUrl) {
+  let results;
+  try {
+    await fetchAndRotateImage(imageUrl);
+    await fetchOBJFile(objUrl);
+
+    const imageFilePath = "tempImage.png";
+    const imageDataUrl = readFileAsDataURL(imageFilePath);
+
+    const objFilePath = "tempObj.obj";
+    const objDataUrl = readObjAsDataURL(objFilePath);
+
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+
+    await page.addScriptTag({
+      url: "https://cdn.jsdelivr.net/npm/@mediapipe/face_detection@0.4.1646425229/face_detection.min.js",
+    });
+    await page.addScriptTag({
+      url: "https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@0.4.1633559619/face_mesh.min.js",
+    });
+    await page.addScriptTag({
+      url: "https://cdnjs.cloudflare.com/ajax/libs/three.js/87/three.min.js",
+    });
+    await page.addScriptTag({
+      url: "https://cdn.rawgit.com/mrdoob/three.js/r87/examples/js/loaders/OBJLoader.js",
+    });
+
+    const scriptContent = fs.readFileSync(path.join(__dirname, "process.js"), {
+      encoding: "utf8",
+    });
+    await page.evaluate(scriptContent);
+
+    const results = await page.evaluate(
+      async (imageDataUrl, objDataUrl) => {
+        return await main(imageDataUrl, objDataUrl);
+      },
+      imageDataUrl,
+      objDataUrl
+    );
+
+    // await browser.close();
+  } catch (error) {
+    console.error("Error in calculateLandmarkDistances: ", error);
+    await new Promise((resolve) => setTimeout(resolve, 100000));
+  }
+
+  return results;
+}
+
+// Example usage
+const IMAGE_URL =
+  "https://ins-ai-speech.s3.ap-northeast-2.amazonaws.com/prod/v2/M06/S2501/F001/M06_S2501_F001.png";
+const OBJ_URL =
+  "https://ins-ai-speech.s3.ap-northeast-2.amazonaws.com/prod/v2/M06/S2501/F001/M06_S2501_F001.obj";
+
+calculateLandmarkDistances(IMAGE_URL, OBJ_URL).then((results) => {
+  console.log("Landmark distances:", results);
+});
